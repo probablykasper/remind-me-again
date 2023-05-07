a47de3f5a000ed7bd8b36a97dbebd7e9c88ccd30
Kasper <kasperkh.kh@gmail.com>
2023-05-07 05:10:16 +0200

    Fix linux/windows builds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -52,7 +52,8 @@ fn main() {
     }
   };
 
-  if !cfg!(debug_assertions) {
+  #[cfg(all(debug_assertions, target_os = "macos"))]
+  {
     rust_macios::user_notifications::UNUserNotificationCenter::current_notification_center()
       .request_authorization_with_options_completion_handler(
         &[
